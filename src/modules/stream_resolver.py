5c1dd1668a6a4c9825a1314b7d2cd9f9a3ced688
Sergey Malinkin <malinkinsa@yandex.ru>
2022-01-08 22:48:41 +0300

    Added error handling. In case of an incorrect response from the api, the default template will be returned

--- src/modules/stream_resolver.py
+++ src/modules/stream_resolver.py
@@ -1,5 +1,7 @@
 import httpx
 
+from src.main import logger
+
 
 class Resolver:
     def __init__(self, graylog_url, auth_type, token=None, login=None, password=None):
@@ -23,4 +25,12 @@ class Resolver:
                 auth=self.auth
             )
 
-        return stream_resolving.json()
+        if stream_resolving.status_code != 200:
+            logger.logging(
+                f'Unable to resolve stream uid to name. Answer {stream_resolving.status_code}',
+                logger_level='ERROR',
+                source='stream_resolver',
+            )
+            return {'description': 'AlertGateway'}
+        else:
+            return stream_resolving.json()
